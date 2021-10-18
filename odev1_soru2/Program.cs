dc463644e70fd5ce614b0d986c59f6ba503b10a5
MrCPU64 <76919938+MrCPU64@users.noreply.github.com>
2021-10-18 20:27:08 +0300

    Add files via upload

---
+++ odev1_soru2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace soru2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen iki pozitif sayı giriniz: ");
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            int m = Convert.ToInt32(Console.ReadLine().Trim());
+            int[] sayilar = new int[n];
+            Console.WriteLine("Lütfen {0} adet pozitif sayı giriniz: ",n);
+            for (int i = 0; i < n; i++)
+            {
+                sayilar[i] = Convert.ToInt32(Console.ReadLine().Trim());
+            }
+            Console.WriteLine("İkinci girilen sayıya eşit ya da tam bölünen sayılar: ");
+            foreach (var item in sayilar)
+            {
+                if(item == m || item%m == 0)
+                    Console.WriteLine(item);
+            }
+        }
+    }
+}
