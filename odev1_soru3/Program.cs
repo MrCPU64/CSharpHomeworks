08fee53ab194ea880e065ded45d10d26a920101a
MrCPU64 <76919938+MrCPU64@users.noreply.github.com>
2021-10-18 20:27:39 +0300

    Add files via upload

---
+++ odev1_soru3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace soru3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen pozitif bir sayı giriniz: ");
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            Console.WriteLine("Lütfen {0} adet kelime giriniz: ",n);
+            string[] kelimeler = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                kelimeler[i] = Console.ReadLine().Trim();
+
+            }
+
+            Console.WriteLine("Kullanıcının girmiş olduğu kelimelerin sondan başa doğru yazılması: ");
+            for (int i = n-1; i >= 0; i--)
+            {
+                Console.WriteLine(kelimeler[i]);
+                
+            }
+
+        }
+    }
+}
