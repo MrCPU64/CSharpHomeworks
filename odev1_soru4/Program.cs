d12072cdeac5114fcc80704464318304e0c293f8
MrCPU64 <76919938+MrCPU64@users.noreply.github.com>
2021-10-18 20:28:11 +0300

    Add files via upload

---
+++ odev1_soru4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace soru4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir cümle yazınız: ");
+            string cumle = Console.ReadLine();
+            int countKelime = 1;
+            int countHarf = 0;
+            for (int i = 0; i < cumle.Length; i++)
+            {
+                if(cumle[i] == ' ')
+                    countKelime++;
+            }
+            Console.WriteLine("Kelime sayısı: "+countKelime);
+
+            for (int j = 0; j < cumle.Length; j++)
+            {
+                if(cumle[j] == ' ')
+                    continue;
+                else
+                    countHarf++;
+
+            }
+
+            Console.WriteLine("Harf sayısı: "+countHarf);
+            
+            
+        }   
+            
+    }
+}
